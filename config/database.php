b7901cc229f7c6f3a135e5f3744bccd09a5f9d9b
DESKTOP-QN6Q0SM\Yusda Helmani <yusdahelmani@gmail.com>
2022-01-31 09:00:29 +0800

    'conection'

--- config/database.php
+++ config/database.php
@@ -84,6 +84,27 @@ return [
         ],
 
 
+        'mysql_apik' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE_APIK_PRESENSI', 'forge'),
+            'username' => env('DB_USERNAME_APIK_PRESENSI', 'forge'),
+            'password' => env('DB_PASSWORD_APIK_PRESENSI', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
